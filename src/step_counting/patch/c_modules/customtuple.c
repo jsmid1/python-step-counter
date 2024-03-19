e643a94726edec711cc27b11e4da3bcd4b72bfb5
Jan Smid <xsmid2@fi.muni.cz>
2024-03-19 20:56:44 +0100

    Fixes tuple.__getitem__

--- src/step_counting/patch/c_modules/customtuple.c
+++ src/step_counting/patch/c_modules/customtuple.c
@@ -16,8 +16,8 @@ static Py_ssize_t patched_tuple_len(PyObject *self) {
 // Need to fix the result returning.
 // May be correct but do not know how to test yet. Getitem should always return PyObject*.
 // ^ https://hg.python.org/cpython/file/04f714765c13/Objects/abstract.c#l1498 Should be correct
-static PyObject *patched_tuple_getitem(PyObject *self, PyObject* elem) {
-    PyObject* result = PyObject_CallFunction(tuple_getitem_patched_method, "(OO)", self, elem);
+static PyObject *patched_tuple_getitem(PyObject *self, long elem) {
+    PyObject* result = PyObject_CallFunction(tuple_getitem_patched_method, "(Ol)", self, elem);
 
     return result;
 }
@@ -61,10 +61,10 @@ static PyObject* patch_tuple_getitem(PyObject* self, PyObject* args) {
     Py_XINCREF(patched_method); // Increase reference count
 
     tuple_getitem_patched_method = patched_method;
+//
+    PyTuple_Type.tp_as_sequence->sq_item = patched_tuple_getitem;
 
-    //PyTuple_Type.tp_as_sequence->sq_item = patched_tuple_getitem;
-
-    PyTuple_Type.tp_as_mapping->mp_subscript = patched_tuple_getitem;
+    //PyTuple_Type.tp_as_mapping->mp_subscript = patched_tuple_getitem;
 
     Py_RETURN_NONE;
 }
