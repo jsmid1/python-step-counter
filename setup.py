21a055982e4be114910a54f03baa137e8bd0c634
Jan Smid <xsmid2@fi.muni.cz>
2024-03-27 22:18:09 +0100

    Formats setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup, Extension, find_packages
 
 # Define the extension module
-patch_modules = \
-[
-    Extension('patchint', sources=['./src/step_counting/patch/c_modules/customint.c'], extra_compile_args=['-g', '-O0']),
-    Extension('patchtuple', sources=['./src/step_counting/patch/c_modules/customtuple.c'], extra_compile_args=['-g', '-O0']),
-    Extension('patchdictionary', sources=['./src/step_counting/patch/c_modules/customdict.c'], extra_compile_args=['-g', '-O0']),
-    Extension('patchlist', sources=['./src/step_counting/patch/c_modules/customlist.c'], extra_compile_args=['-g', '-O0'])
+patch_modules = [
+    Extension(
+        'patchint',
+        sources=['./src/step_counting/patch/c_modules/customint.c'],
+        extra_compile_args=['-g', '-O0'],
+    ),
+    Extension(
+        'patchtuple',
+        sources=['./src/step_counting/patch/c_modules/customtuple.c'],
+        extra_compile_args=['-g', '-O0'],
+    ),
+    Extension(
+        'patchdictionary',
+        sources=['./src/step_counting/patch/c_modules/customdict.c'],
+        extra_compile_args=['-g', '-O0'],
+    ),
+    Extension(
+        'patchlist',
+        sources=['./src/step_counting/patch/c_modules/customlist.c'],
+        extra_compile_args=['-g', '-O0'],
+    ),
 ]
 
 # Run the setup
